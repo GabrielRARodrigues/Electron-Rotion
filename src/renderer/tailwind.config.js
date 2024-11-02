92ec8d9cebb1c7927426f070c99df662a97ee96d
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-11-02 20:11:46 -0300

    raw: Applying the search functionality for created documents through the components 'Search' and 'SearchBar'. Finalizing the implementations of the 'Search' and 'SearchBar' components in the 'renderer' layer, enabling the search field to allow searching for already created documents

--- src/renderer/tailwind.config.js
+++ src/renderer/tailwind.config.js
@@ -42,6 +42,8 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
+    require('tailwind-scrollbar'),
+
     plugin(({ addUtilities }) => {
       addUtilities({
         '.region-drag': {
